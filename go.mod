9425f90698d976235424341be5a77dedcc81c037
fajarbc <bcfajar@gmail.com>
2021-11-01 22:56:15 +0700

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fajarbc/go-say-hello-app
+
+go 1.17
+
+require github.com/fajarbc/go-say-hello v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	sayhello "github.com/fajarbc/go-say-hello"
+)
+
+func main() {
+	fmt.Println(sayhello.SayHello())
+}
